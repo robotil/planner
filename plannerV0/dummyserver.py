55f4ab5f77feed9164f99bce452407b158a4ebfa
michele@apollo <mhallakstamler@gmail.com>
2020-07-30 15:09:39 +0300

    Unsatisfactory services implementation But works.

--- plannerV0/dummyserver.py
+++ plannerV0/dummyserver.py
@@ -9,15 +9,23 @@ from planner_msgs.msg import SDiagnosticStatus, SGlobalPose, SHealth, SImu, Enne
 from planner_msgs.srv import ActGeneralAdmin, StateGeneralAdmin, CheckLOS
 
 class DummyServer(Node):
-
     def __init__(self):
         super().__init__('dummy_server')
-        self.srv = self.create_service(ActGeneralAdmin, 'act_general_admin', self.act_general_admin_callback)        # CHANGE
+        self.state = bytes([0])
+        self.actAdminSrv = self.create_service(ActGeneralAdmin, 'act_general_admin', self.act_general_admin_callback)
+        self.stateAdminSrv = self.create_service(StateGeneralAdmin, 'state_general_admin', self.state_general_admin_callback)
 
     def act_general_admin_callback(self, request, response):
-        response.resulting_status = request.admin                                               # CHANGE
+        response.resulting_status = request.admin
         self.get_logger().info('Incoming request\na: adm: %d' % (int.from_bytes(request.admin, "big")))
+        self.state = request.admin
         return response
+
+    def state_general_admin_callback(self, request, response):
+        self.get_logger().info('Returning state\n state: %d' % (int.from_bytes(self.state, "big")))
+        response.resulting_status = self.state
+        return response
+
 
 def main(args=None):
     rclpy.init(args=args)
--- plannerV0/worldcom.py
+++ plannerV0/worldcom.py
@@ -1,5 +1,6 @@
 import rclpy
 from rclpy.node import Node
+from rclpy.callback_groups import ReentrantCallbackGroup
 
 from std_msgs.msg import String
 from diagnostic_msgs.msg import DiagnosticStatus
@@ -44,10 +45,27 @@ class WorldCom(Node):
         if self.act_req == 0:
             while not self.genAdminCli.wait_for_service(timeout_sec=1.0):
                 self.get_logger().info('service ActGeneralAdmin not available, waiting ...')
-            self.act_req = ActGeneralAdmin.Request()
+        self.act_req = ActGeneralAdmin.Request()
 
         self.act_req.admin = bytes([command])
         self.future = self.genAdminCli.call_async(self.act_req)
+
+    def state_gen_admin_request(self):
+        if self.stat_req == 0:
+            while not self.stateAdminCli.wait_for_service(timeout_sec=1.0):
+                self.get_logger().info('service StateGeneralAdmin not available, waiting ...')
+            self.stat_req = StateGeneralAdmin.Request()
+        self.future = self.stateAdminCli.call_async(self.stat_req)
+        self.get_logger().info('state_gen_admin_request sent, waiting ...')
+        # rclpy.spin_once(self)
+        # while not self.future.done():
+        #     continue
+        # try:
+        #     response = self.future.result()
+        # except Exception as e:\
+        #     self.get_logger().info('Service call failed %r' % (e,))
+        # else:
+        #     self.get_logger().info('Result of state_gen_admin_request: %d' % (response.resulting_status))
 
     def timer_callback(self):
         self.get_logger().info('Sending: "%d"' % self.i)
@@ -57,6 +75,7 @@ class WorldCom(Node):
             self.i=0
         else:
             self.i += 1
+        self.state_gen_admin_request()
 
     def global_pose_callback(self, msg):
         self.get_logger().info('Received: "%s"' % msg)
@@ -87,6 +106,16 @@ def main(args=None):
     rclpy.init(args=args)
 
     world_communication = WorldCom()
+
+    while rclpy.ok():
+        rclpy.spin_once(world_communication)
+        if world_communication.future.done():
+            try:
+                response = world_communication.future.result()
+            except Exception as e:
+                world_communication.get_logger().info('Service call failed %r' % (e,))
+            else:
+                world_communication.get_logger().info('Result of state_gen_admin_request: %d' % (int.from_bytes(response.resulting_status, "big")))
 
     rclpy.spin(world_communication)
     # Destroy the node explicitly
