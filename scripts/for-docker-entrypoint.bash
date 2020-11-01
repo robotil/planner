f14af0f19939e85f6654e4501ba8d530405a9dc6
iai <iai@iaiai>
2020-11-01 19:16:21 +0200

    Petit a petit

--- scripts/for-docker-entrypoint.bash
+++ scripts/for-docker-entrypoint.bash
@@ -5,3 +5,9 @@ echo install/setup.bash >> ~/.bashrc
 cd /home/iaiai/git/ros2ws/src/planner
 cp -r logic_simulator  /home/iaiai/git/ros2ws/install/planner/lib/python3.6/site-packages/
 cp -r planner/envs  /home/iaiai/git/ros2ws/install/planner/lib/python3.6/site-packages/planner
+
+if [ -z "$1" ]
+  then
+    echo "no domain id - default"
+fi
+export ROS_DOMAIN_ID="$1"
